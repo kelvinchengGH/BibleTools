a0d55b8cbd0900a9ec1e73e22ce468bc01d10f87
kcheng <kcheng@arista.com>
2022-11-16 20:44:35 -0800

    Clean up VerseOfTheDay.py

--- VerseOfTheDay.py
+++ VerseOfTheDay.py
@@ -1,36 +1,65 @@
+#!/usr/bin/python
+
 """
 Fetches the Verse of the Day from Bible Gateway
 and prints it to the terminal.
 """
 
+
+#############
+# IMPORTS
+#############
 import os, subprocess, textwrap
 from bs4 import BeautifulSoup
 
 
-os.system( 'clear' )
+#############
+# CONSTANTS
+#############
+LINE_LEN = 60
+URL = 'https://www.biblegateway.com'
 
 
-# Get the HTML for the site.
-url = 'https://www.biblegateway.com'
-cmd = [ 'curl', '-s', url ]
-output = subprocess.check_output( cmd )
+#############
+# FUNCTIONS
+#############
+def getVerseOfTheDay():
+    """ Fetch the verse of the day and its citation. """
+    # Get the HTML for the site.
+    cmd = [ 'curl', '-s', URL ]
+    output = subprocess.check_output( cmd )
+
+    # Parse it to get the verse of the day and the citation.
+    soup = BeautifulSoup( output, 'html.parser' )
+    
+    citation = soup.find( 'span', { 'class' : 'citation' } )
+    citationStr = citation.text
+    
+    verse = soup.find( 'div', { 'id' : 'verse-text' } )
+    verseStr = verse.text.strip()
+
+    return ( verseStr, citationStr )
 
 
-# Parse it to get the verse of the day and the citation.
-soup = BeautifulSoup( output, 'html.parser' )
+def printVerse( verse, citation ):
+    """ Print the verse in a pretty format. """
+    print '~' * LINE_LEN
+    print '\n'.join( textwrap.wrap( verse, LINE_LEN,
+                                    break_long_words=False ) )
+    print
+    print citation
+    print '~' * LINE_LEN
 
-citation = soup.find( 'span', { 'class' : 'citation' } )
-citationStr = citation.text
 
-verse = soup.find( 'div', { 'id' : 'verse-text' } )
-verseStr = verse.text.strip()
+#############
+# main()
+#############
+def main():
+    os.system( 'clear' )
+    verse, citation = getVerseOfTheDay()
+    printVerse( verse, citation )
 
+    
+if __name__ == "__main__":
+    main()
 
-# Print the verse in a pretty format.
-lineLen = 60
-print '~' * lineLen
-print '\n'.join( textwrap.wrap( verseStr, lineLen,
-                                break_long_words=False ) )
-print
-print citationStr
-print '~' * lineLen
